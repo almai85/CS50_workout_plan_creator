b46d5cb3edf903b5fc0fee282e219c92109846fe
Alexander Maier <alexander2.maier@audi.de>
2024-05-06 19:13:38 +0200

    modified some Bootstrap

--- app.py
+++ app.py
@@ -6,7 +6,11 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-print(__name__)
+
+@app.route("/test")
+def test():
+    return render_template("test.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
